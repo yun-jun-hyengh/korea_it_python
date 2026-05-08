64d07e0da7b2fa42b72b4530e0d92e0bd413b395
yun-jun-hyengh <77119415+yun-jun-hyengh@users.noreply.github.com>
2026-05-08 23:49:27 +0900

    2026-05-08

--- Day02/ex03.py
+++ Day02/ex03.py
@@ -2,3 +2,13 @@
 float(x)
 - 인자로 들어온 x를 실수 타입으로 변환해준다 
 '''
+
+a = 10;
+a1 = float(a);
+print("a : ", a); print("a : ", type(a));
+print("a1 : ", a1); print("a1 : ", type(a1));
+
+b = True;
+b1 = float(b);
+print("b : ", b); print("b : ", type(b));
+print("b1 : ", b1); print("b1 : ", type(b1));
