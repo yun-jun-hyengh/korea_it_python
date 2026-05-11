611ae0c7b16b935582bb4be23135adef32cd6a98
yun-jun-hyengh <77119415+yun-jun-hyengh@users.noreply.github.com>
2026-05-11 11:00:58 +0900

    2026-05-11

--- Day04/ex02.py
+++ Day04/ex02.py
@@ -4,3 +4,20 @@
 age = 20;
 if age >= 20:
     print("20세 이상입니다.");
+
+# 논리 연산자와 함께 제어문 구현
+# 국영수 점수가 모두 60점 이상이면 합격
+kor = 100;
+eng = 85;
+math = 95;
+
+if kor >= 60 and eng >= 60 and math >= 60:
+    print("합격입니다.");
+else:
+    print("불합격입니다.");
+
+# 국영수 점수 중 하나라도 60점 미만이면 불합격 
+if kor < 60 or eng < 60 or math < 60:
+    print("불합격입니다.");
+else:
+    print("합격입니다.");
